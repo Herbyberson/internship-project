0a3953eb30e4ec221e72b8c971436dfe1118d877
Herby Berson <Berson.Herby@gmail.com>
2024-03-20 21:26:00 -0400

    Headless Google Chrome

--- features/environment.py
+++ features/environment.py
@@ -1,20 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from app.application import Application
 
+
 def browser_init(context):
     """
     :param context: Behave context
     """
+    # headless environment Google Chrome set up:
+
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
     driver_path = ChromeDriverManager().install()
     service = Service(driver_path)
-    context.driver = webdriver.Chrome(service=service)
-
-    context.driver.maximize_window()
+    context.driver = webdriver.Chrome(service=service, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
